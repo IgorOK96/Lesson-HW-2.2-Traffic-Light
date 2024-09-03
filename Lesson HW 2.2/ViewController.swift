f34b847eda322d706baf40793c052d47d519c9cd
IgorOK96 <IgorOK96@users.noreply.github.com>
2024-09-03 16:34:28 +0200

    Initial Commit

---
+++ Lesson HW 2.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson HW 2.2
+//
+//  Created by user246073 on 9/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
