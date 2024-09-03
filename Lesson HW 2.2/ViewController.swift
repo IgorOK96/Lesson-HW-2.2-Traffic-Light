e8d47d39f77f3232492caafa96830f92b0dd93d8
IgorOK96 <IgorOK96@users.noreply.github.com>
2024-09-03 21:59:32 +0200

    good job 2.1

--- Lesson HW 2.2/ViewController.swift
+++ Lesson HW 2.2/ViewController.swift
@@ -9,11 +9,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var turnOnButton: UIButton!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var redLight: UIView!
+    
+//    var workLight = 0
+  
+    enum CollorTraffic {
+        case red, yellow, green
+    }
+    
+    var currentState: CollorTraffic =  .red
+    
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        turnOnButton.layer.cornerRadius = 10
+        greenLight.layer.cornerRadius = 55
+        redLight.layer.cornerRadius = 55
+        yellowLight.layer.cornerRadius = 55
+        
+        offTrafficLight()
+
     }
 
+    
 
+    @IBAction func turnOnTrafficLight(_ sender: UIButton) {
+        turnOnButton.setTitle("NEXT", for: .normal)
+              
+        switch currentState {
+            
+        case .red:
+            offTrafficLight()
+            redLight.alpha = 1
+            currentState = .yellow
+        case .yellow:
+            offTrafficLight()
+            yellowLight.alpha = 1
+            currentState = .green
+        case .green:
+            offTrafficLight()
+            greenLight.alpha = 1
+            currentState = .red
+        }
+    }
+    private func offTrafficLight() {
+        redLight.alpha = 0.5
+        yellowLight.alpha = 0.5
+        greenLight.alpha = 0.5
+    }
 }
+    
+
+
 
